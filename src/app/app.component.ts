7773e0a804adae3e94317045fb574e920dad8f12
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-09 22:54:36 +0100

    bad usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { GlitchFreeComponent } from "./signals/glitch-free/glitch-free.component";
 import { IsEvenComponent } from './signals/is-even/is-even.component';
 import { SignalSearchComponent } from './signals/signal-search/signal-search.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SignalSearchComponent, IsEvenComponent, GlitchFreeComponent],
+  imports: [SignalSearchComponent, IsEvenComponent, GlitchFreeComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  arr = signal([]) as any
 
   counterValue = 5;
 
   ngOnInit() {
     setTimeout(() => {
       this.counterValue = 10
+      this.arr().push('run')
     }, 3000);
   }
 
