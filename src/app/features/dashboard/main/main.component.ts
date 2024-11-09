de9610a64710f01c25b8ecac3ee2abb170a2a98d
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-09 13:34:23 +0100

    control flow

--- src/app/features/dashboard/main/main.component.ts
+++ src/app/features/dashboard/main/main.component.ts
@@ -1,12 +1,33 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, NgSwitchCase, NgSwitch, NgSwitchDefault],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  decision = true
+  weight = 251
+  tv = 'prima'
+
+  members = [
+    // {
+    //   id: 'uuid1',
+    //   name: 'Karel',
+    //   dateOfBirth: '22.11.1990'
+    // },
+    // {
+    //   id: 'uuid2',
+    //   name: 'Petr',
+    //   dateOfBirth: '22.11.1970'
+    // }
+  ]
+
+  trackByMemberId(index: number, member: any): string {
+    return member.id;
+  }
 
 }
