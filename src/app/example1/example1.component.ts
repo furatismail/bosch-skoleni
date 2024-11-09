5681d73e117fce9d4c7f631a0f40c8ae96405c31
Furát Ismail <furat.ismail@cadcom.cz>
2024-11-09 21:48:14 +0100

    init

--- src/app/example1/example1.component.ts
+++ src/app/example1/example1.component.ts
@@ -9,27 +9,47 @@ import { Component, computed, effect, signal } from '@angular/core';
   styleUrls: ['./example1.component.css'] // Corrected the property name from styleUrl to styleUrls
 })
 export class Example1Component {
-  search = signal(localStorage.getItem('searchString') || '');
-  users = signal([
-    { id: 1, name: "Carl" },
-    { id: 2, name: 'Peter' }
-  ])
-  filteredUsers = computed(() => this.users().filter((u) => u.name.toLowerCase().startsWith(this.search())))
+  // search = signal(localStorage.getItem('searchString') || '');
+  // users = signal([
+  //   { id: 1, name: "Carl" },
+  //   { id: 2, name: 'Peter' }
+  // ])
+  // filteredUsers = computed(() => this.users().filter((u) => u.name.toLowerCase().startsWith(this.search())))
 
-  logger = effect(() => {
-    localStorage.setItem('searchString', this.search())
-  })
+  // logger = effect(() => {
+  //   localStorage.setItem('searchString', this.search())
+  // })
 
 
-  setSearchString(e: Event) {
-    this.search.set((e.target as HTMLInputElement).value.toLowerCase());
+  // setSearchString(e: Event) {
+  //   this.search.set((e.target as HTMLInputElement).value.toLowerCase());
+  // }
+
+  // addUser() {
+  //   this.users.update((users) => [...users, { id: 3, name: "John" }]);
+
+  //   // destroy
+  //   // this.logger.destroy()
+  // }
+
+  showCount = false;
+  count = signal(0);
+  conditionalCount = computed(() => {
+    if (this.showCount) {
+      return `The count is ${this.count()}.`;
+    } else {
+      return 'Nothing to see here!';
+    }
+  });
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(11);
+      this.showCount = true
+
+      console.log(this.count())
+    }, 3000);
   }
-
-  addUser() {
-    this.users.update((users) => [...users, { id: 3, name: "John" }]);
-
-    // destroy
-    // this.logger.destroy()
-  }
+  
 
 }
